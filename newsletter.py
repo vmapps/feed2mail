a1f273976a29ebe686d309ef4a7d59dfbaa543c7
VM Apps <31423375+vmapps@users.noreply.github.com>
2019-05-18 11:13:26 +0200

    introduce JSON config file

--- newsletter.py
+++ newsletter.py
@@ -7,34 +7,34 @@ import sys
 import utils
 
 with open( os.path.dirname(__file__)+'/config.json','r') as fh:
-  cfg = json.load(fh)
+  config = json.load(fh)
 fh.close()
 
-if cfg.get('feeds.sources'):
+if config.get('feeds.sources'):
   feed = utils.feed()
   feed.header()
 
-  for idx,url in enumerate( cfg['feeds.sources'] ):
-    sys.stderr.write( '[%0d/%0d]\tRetrieving %s ...\n' %(idx+1,len(cfg['feeds.sources']),url) )
+  for idx,url in enumerate( config['feeds.sources'] ):
+    sys.stderr.write( '[%0d/%0d]\tRetrieving %s ...\n' %(idx+1,len(config['feeds.sources']),url) )
 
     feed.get(url)
-    feed.parse( cfg['feeds.period'] )
+    feed.parse( config['feeds.period'] )
 
-  feed.footer( cfg['feeds.sources'] )
+  feed.footer( config['feeds.sources'] )
 
-  if cfg.get('debug'):
+  if config.get('debug'):
     print feed.html.encode('utf8')
     # exit()
 else:
   sys.stderr.write( '[ERROR] no feed sources' )
   sys.exit(0)
 
-if cfg.get('email.recipients'):
+if config.get('email.recipient'):
   dt1 = datetime.datetime.today().strftime('%d/%m/%Y')
   dt2 = datetime.datetime.today().strftime('%Y%m%d')
 
-  e = utils.email( cfg['smtp.host'], cfg['smtp.port'], cfg['smtp.ssl'], cfg['smtp.user'], cfg['smtp.pass'] )
-  e.header( cfg['email.sender'], cfg['email.recipient'], '%s %s' % (cfg['email.title'],dt1) )
+  e = utils.email( config['smtp.host'], config['smtp.port'], config['smtp.ssl'], config['smtp.user'], config['smtp.pass'] )
+  e.header( config['email.sender'], config['email.recipient'], '%s %s' % (config['email.title'],dt1) )
   e.body( feed.html.encode('utf8') )
   e.attachment( feed.html.encode('utf8'), 'newsletter-%s.html' % dt2 )
   e.send()
